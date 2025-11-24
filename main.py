690f7a87b970cf86a16c4364e9ce4604864e6f55
fatema-nagy <willaiko93@gmail.com>
2025-11-24 04:27:48 +0200

    hello world chain

---
+++ main.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    information = """ Nikola Tesla[a] (10 July 1856 – 7 January 1943) was a Serbian-American engineer, futurist, and inventor. He is known for his contributions to the design of the modern alternating current (AC) electricity supply system.[2]
+
+Born and raised in the Austrian Empire, Tesla first studied engineering and physics in the 1870s without receiving a degree. He then gained practical experience in the early 1880s working in telephony and at Continental Edison in the new electric power industry. In 1884, he immigrated to the United States, where he became a naturalized citizen. He worked for a short time at the Edison Machine Works in New York City before he struck out on his own. With the help of partners to finance and market his ideas, Tesla set up laboratories and companies in New York to develop a range of electrical and mechanical devices. His AC induction motor and related polyphase AC patents, licensed by Westinghouse Electric in 1888, earned him a considerable amount of money and became the cornerstone of the polyphase system, which that company eventually marketed.
+
+Attempting to develop inventions he could patent and market, Tesla conducted a range of experiments with mechanical oscillators/generators, electrical discharge tubes, and early X-ray imaging. He also built a wirelessly controlled boat, one of the first ever exhibited. Tesla became well known as an inventor and demonstrated his achievements to celebrities and wealthy patrons at his lab, and was noted for his showmanship at public lectures. Throughout the 1890s, Tesla pursued his ideas for wireless lighting and worldwide wireless electric power distribution in his high-voltage, high-frequency power experiments in New York and Colorado Springs. In 1893, he made pronouncements on the possibility of wireless communication with his devices. Tesla tried to put these ideas to practical use in his unfinished Wardenclyffe Tower project, an intercontinental wireless communication and power transmitter, but ran out of funding before he could complete it.
+
+After Wardenclyffe, Tesla experimented with a series of inventions in the 1910s and 1920s with varying degrees of success. Having spent most of his money, Tesla lived in a series of New York hotels, leaving behind unpaid bills. He died in New York City in January 1943.[3] Tesla's work fell into relative obscurity following his death, until 1960, when the General Conference on Weights and Measures named the International System of Units (SI) measurement of magnetic flux density the tesla in his honor. There has been a resurgence in popular interest in Tesla since the 1990s.[4] In 2013, Time named Tesla one of the 100 most significant figures of all time.[5]
+
+"""
+
+    summary_template = """
+    given the information {information} about a person i want you to create:
+    1- A short summary
+    2- two intersting facts about them """
+
+    summary_prompt_template = PromptTemplate(
+        input_variable=[information], template=summary_template
+    )
+    #llm = ChatOpenAI(temperature=0, model="gpt-5", max_tokens=1000)
+    #llm = ChatOllama(temperature=0, model="gemma3:270m",  base_url="http://172.22.112.1:11434")
+    llm = ChatOllama(temperature=0, model="gpt-oss:20b",  base_url="http://172.22.112.1:11434")
+    
+    chain = summary_prompt_template | llm
+    response = chain.invoke({"information": information})
+    print(response.content)
+
+
+if __name__ == "__main__":
+    main()
