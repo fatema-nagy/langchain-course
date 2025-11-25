833cd5f66ee88caae192d15c381dbe2fcde0dd24
fatema-nagy <willaiko93@gmail.com>
2025-11-25 07:56:23 +0200

    agent with pydantic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
@@ -11,10 +12,19 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and source """
+    answer:str = Field(description="The agent answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 
 llm = ChatOpenAI()
-tools = [TavilySearch]
-agent = create_agent(model=llm, tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
